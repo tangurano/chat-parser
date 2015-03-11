92416096566071ba8cfbba44204d29892d70d2a5
Charles Tang <charles.tang@utexas.edu>
2015-03-11 14:23:36 -0500

    Handle emoticons and new corner cases
    Handled some corner cases to avoid empty emoticons/mentions
    Added support for emoticons

--- parser.js
+++ parser.js
@@ -7,18 +7,22 @@ function parseMessage(message) {
 
 	metadata = {};
 	for (word of words) {
-		// TODO: parse for mentions, emoticons, and links as we go
-		// TODO: make the code more modular/extensible to parse additional types of words
+		// TODO: refactor the below code to be more modular/extensible and allow easy addition of new types of words 
+		// prefixed (@, links) and encapsulated (parenthesis)
 		if (word.length > 0) {
-			if (word[0] === '@') { 
+			if (word[0] === '@' && word.length > 1) { 
 				var user = word.slice(1) //assumes validation of users is done by another module
 				if (metadata.hasOwnProperty('mentions'))
 					metadata.mentions.push(user);
-				else 
+				else
 					metadata.mentions = [user];
 			}
-			if (word[0] === '(' && word[word.length - 1] === ')') {
-				//TODO: add to emoticons
+			if (word[0] === '(' && word[word.length - 1] === ')' && word.length > 2) {
+				var emoticon = word.slice(1, -1) //assumes validation of emoticons is done by another module
+				if (metadata.hasOwnProperty('emoticons'))
+					metadata.emoticons.push(emoticon);
+				else
+					metadata.emoticons = [emoticon];
 			}
 			if (word.indexOf('https://') === 0 || word.indexOf('http://') === 0) { 
 				//assume links are always prefixed with https:// ? //maybe handle www also?
@@ -75,9 +79,14 @@ var mixed_json = {
 // mapping input msgs to expected outputs
 var test_cases = {
 
-	//TODO: add more corner cases
+	// corner cases
 	"": {},
 
+	"@ @ @" : {},
+
+	"() () ()": {},
+
+	// common cases
 	"this is a plaintext message": {},
 
 	"@chris you around?" : mentions_json,
@@ -87,8 +96,9 @@ var test_cases = {
 	"Olympics are starting soon; http://www.nbcolympics.com": links_json,
 
 	"@bob @john (success) such a cool feature; https://twitter.com/jdorfman/status/430511497475670016": mixed_json,
-}
 
+	// TODO: test mixing multiple of each type
+}
 
 // test cases
 // console.log(parseMessage("hello, world"));
@@ -97,6 +107,7 @@ for (var msg in test_cases) {
   if (test_cases.hasOwnProperty(msg)) {
   	var expected = JSON.stringify(test_cases[msg]);
   	var actual = parseMessage(msg);
+
   	if (expected === actual)
   		console.log('PASS: ' + msg);
   	else {
